4e6583c4cabecf41a01171cf8932a05d002890d5
ryanjesus <ryan.jesus@hotmail.com>
2023-04-13 22:51:11 -0300

    FilmesAPI

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,23 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers;
+
+//Atalho de importação alt + enter
+
+[ApiController] //Sempre quando usa o ApiController importa a using Microsoft.AspNetCore.Mvc;
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+	private static List<Filme> filmes = new List<Filme>();
+
+	[HttpPost]
+	public void AdicionaFilme([FromBody]Filme filme) //O tipo é void pq n vou me preocupar com o retorno
+							//Estamos recebendo o filme atraves do parametro	
+	{
+		filmes.Add(filme);
+
+		Console.WriteLine(filme.Titulo);
+		Console.WriteLine(filme.Duracao);
+	}
+}
---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,11 @@
+﻿namespace FilmesAPI.Models;
+
+public class Filme
+{
+	//prop é um atalho para criar atributo
+	public string Titulo { get; set; }
+
+	public string Genero { get; set; }
+
+	public int Duracao { get; set; }
+}
