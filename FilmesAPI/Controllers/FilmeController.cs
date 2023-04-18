1adcc6bdcd61f77138f332ffc0c9014efc377c22
ryanjesus <ryan.jesus@hotmail.com>
2023-04-18 20:46:20 -0300

    Atualização de dados com path e put

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -2,6 +2,7 @@
 using FilmesAPI.Data;
 using FilmesAPI.Data.Dtos;
 using FilmesAPI.Models;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FilmesAPI.Controllers;
@@ -46,5 +47,34 @@ public class FilmeController : ControllerBase
 		var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
 		if (filme == null) return NotFound();
 		return Ok(filme);
+	}
+
+	[HttpPut("{id}")]
+	public IActionResult AtualizaFilme(int id, [FromBody] UpdateFilmeDto filmeDto)
+	{
+		var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+		if(filme == null) return NotFound();
+		_mapper.Map(filmeDto, filme);
+		_context.SaveChanges();
+		return NoContent();
+	}
+
+	[HttpPatch("{id}")]
+	public IActionResult AtualizaFilmeParcial(int id, JsonPatchDocument<UpdateFilmeDto> patch)
+	{
+		var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+		if (filme == null) return NotFound();
+
+		var filmeParaAtualizar = _mapper.Map<UpdateFilmeDto>(filme);
+
+		patch.ApplyTo(filmeParaAtualizar, ModelState);
+		if (!TryValidateModel(filmeParaAtualizar))
+		{
+			return ValidationProblem(ModelState);
+		}
+
+		_mapper.Map(filmeParaAtualizar, filme);
+		_context.SaveChanges();
+		return NoContent();
 	}
 }
---
+++ FilmesAPI/Data/Dtos/UpdateFilmeDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Data.Dtos;
+
+public class UpdateFilmeDto
+{
+
+	//prop é um atalho para criar atributo
+	[Required(ErrorMessage = "O título do filme é obrigatório")] //Deixando o campo obrigatório
+	[StringLength(50, ErrorMessage = "O titulo do filme não pode exceder 50 caracteres")]
+	public string Titulo { get; set; }
+
+	[Required(ErrorMessage = "O título do gênero é obrigatório")]
+	[StringLength(50, ErrorMessage = "O tamanho do gênero não pode exceder 50 caracteres")]
+	public string Genero { get; set; }
+
+	[Required(ErrorMessage = "A duração do filme é obrigatório")]
+	[Range(70, 600, ErrorMessage = "A duração deve ter entre 70 e 600 minutos")]
+	public int Duracao { get; set; }
+
+	[Required(ErrorMessage = "O nome do Diretor é obrigatório")]
+	public string Diretor { get; set; }
+}
